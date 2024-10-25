f56b0c4d36770adce3275c113bd54ec01318d8d1
Jash Shah <132209796+1jashshah@users.noreply.github.com>
2024-10-25 18:11:58 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ provider "aws" {
   region = var.region
 }
 
-resource "aws_instance" "example" {
+resource "aws_instance" "my-instance" {
   ami           = var.ami
   instance_type = var.instance_type
   tags = {
@@ -11,9 +11,9 @@ resource "aws_instance" "example" {
 }
 
 output "instance_id" {
-  value = aws_instance.example.id
+  value = aws_instance.my-instance.id
 }
 
 output "instance_public_ip" {
-  value = aws_instance.example.public_ip
+  value = aws_instance.my-instance.public_ip
 }
